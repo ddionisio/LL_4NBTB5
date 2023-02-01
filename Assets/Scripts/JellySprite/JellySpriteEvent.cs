4e4aa1a60a8505ce342482629f848f694d27ff04
David Dionisio <ddionisio@renegadeware.com>
2023-02-01 14:05:26 -0500

    a bunch more initial stuff

---
+++ Assets/Scripts/JellySprite/JellySpriteEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class JellySpriteBaseEvent : UnityEvent<JellySprite, int, BaseEventData> {
+}
+
+[System.Serializable]
+public class JellySpritePointerEvent : UnityEvent<JellySprite, int, PointerEventData> {
+}
+
+[System.Serializable]
+public class JellySpriteAxisEvent : UnityEvent<JellySprite, int, AxisEventData> {
+}
---
+++ Assets/Scripts/JellySprite/JellySpriteEventDrag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Jelly Sprite/Events/Drag")]
+public class JellySpriteEventDrag : JellySpriteEventBase {
+    [Header("Events")]
+    public JellySpritePointerEvent beginDragEvent;
+    public JellySpritePointerEvent dragEvent;
+    public JellySpritePointerEvent endDragEvent;
+    public JellySpritePointerEvent initializePotentialDragEvent;
+
+    protected override void AddCallbacks(JellySpriteEventTrigger eventTrigger) {
+        eventTrigger.beginDragCallback += beginDragEvent.Invoke;
+        eventTrigger.dragCallback += dragEvent.Invoke;
+        eventTrigger.endDragCallback += endDragEvent.Invoke;
+        eventTrigger.initializePotentialDragCallback += initializePotentialDragEvent.Invoke;
+    }
+
+    protected override void RemoveCallbacks(JellySpriteEventTrigger eventTrigger) {
+        eventTrigger.beginDragCallback -= beginDragEvent.Invoke;
+        eventTrigger.dragCallback -= dragEvent.Invoke;
+        eventTrigger.endDragCallback -= endDragEvent.Invoke;
+        eventTrigger.initializePotentialDragCallback -= initializePotentialDragEvent.Invoke;
+    }
+}
